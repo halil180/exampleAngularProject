1d87b915d0257e8d198d3cdbcb6cf1f12756c89f
halil180 <halilkom180@.com>
2022-09-25 16:33:41 +0200

    directives

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  // users:string[] = [];
+
+  users: { name: string, age: number }[] = []
   constructor() { }
 
   ngOnInit(): void {
+    // this.users = ["halil",'john','alex']
+    this.users = [
+      {
+        name:'Halil',
+        age:27
+      },
+      {
+        name:'john',
+        age:23
+      },
+      {
+        name:'alex',
+        age:15
+      }
+    ]
   }
 
 }
